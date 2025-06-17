bbd5a850c03a2b59e1c9716e6f3707b25d8c3b16
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2025-06-17 13:09:33 +0200

    Exponential

---
+++ math/probability/exponential.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Defines the Exponential distribution class"""
+
+
+class Exponential:
+    """Represents an exponential distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """
+        Initializes an exponential distribution.
+
+        Args:
+            data (list): List of data to estimate the distribution.
+            lambtha (float): Expected number of occurrences per time unit.
+
+        Raises:
+            TypeError: If data is not a list.
+            ValueError: If data has fewer than two values.
+            ValueError: If lambtha is not a positive value.
+        """
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            mean = sum(data) / len(data)
+            self.lambtha = 1 / mean
