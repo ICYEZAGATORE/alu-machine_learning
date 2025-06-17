35602b854ec09c685705f0d1fa8a07dd428c555f
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2025-06-17 12:52:48 +0200

    Adding PMF

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Poisson Distribution class"""
-from math import exp, factorial
+"""Poisson Distribution class - no imports allowed"""
 
 
 class Poisson:
@@ -20,16 +19,28 @@ class Poisson:
             self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
-        """Calculates the PMF for a given number of 'successes'
-
-        Args:
-            k: number of 'successes'
-
-        Returns:
-            PMF value for k
-        """
+        """Calculates the PMF for a given number of 'successes'"""
         k = int(k)
         if k < 0:
             return 0
 
-        return (self.lambtha ** k * exp(-self.lambtha)) / factorial(k)
+        return (self.lambtha ** k * self._exp(-self.lambtha)) / self._factorial(k)
+
+    def _factorial(self, n):
+        """Calculates factorial of n (n!)"""
+        if n == 0 or n == 1:
+            return 1
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+
+    def _exp(self, x):
+        """Approximates e^x using a Taylor series"""
+        result = 1.0
+        term = 1.0
+        for i in range(1, 50):  # 50 terms for decent precision
+            term *= x / i
+            result += term
+        return result
+    
