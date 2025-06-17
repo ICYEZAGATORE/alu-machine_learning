b0dd33185adfaae4f1f24e36a96a3b6e4b5e79b2
ICYEZAGATORE <i.icyezagat1@alustudent.com>
2025-06-17 12:58:17 +0200

    Fixing errors

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
+"""Defines the Poisson distribution class"""
+
 
 class Poisson:
     """Represents a Poisson distribution"""
 
     def __init__(self, data=None, lambtha=1.):
+        """
+        Initializes a Poisson distribution.
+
+        Args:
+            data (list): List of data to estimate the distribution.
+            lambtha (float): Expected number of occurrences in a given time frame.
+
+        Raises:
+            TypeError: If data is not a list.
+            ValueError: If data contains fewer than two values.
+            ValueError: If lambtha is not a positive value.
+        """
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -16,6 +30,15 @@ class Poisson:
             self.lambtha = float(sum(data) / len(data))
 
     def pmf(self, k):
+        """
+        Calculates the PMF for a given number of 'successes'.
+
+        Args:
+            k (int): Number of successes
+
+        Returns:
+            float: PMF value for k
+        """
         k = int(k)
         if k < 0:
             return 0
@@ -23,10 +46,35 @@ class Poisson:
         return (self.lambtha ** k * self._exp(-self.lambtha)) / self._factorial(k)
 
     def _factorial(self, n):
+        """
+        Computes factorial of a number.
+
+        Args:
+            n (int): The number
+
+        Returns:
+            int: n!
+        """
         if n == 0 or n == 1:
             return 1
         result = 1
         for i in range(2, n + 1):
             result *= i
         return result
-    
+
+    def _exp(self, x):
+        """
+        Approximates e^x using a Taylor series expansion.
+
+        Args:
+            x (float): The exponent
+
+        Returns:
+            float: Approximate value of e^x
+        """
+        result = 1.0
+        term = 1.0
+        for i in range(1, 50):
+            term *= x / i
+            result += term
+        return result
